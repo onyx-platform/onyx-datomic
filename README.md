f2463865628f5929f00d2e53e3d4b7db786334a8
Michael Drogalis <mjd3089@rit.edu>
2014-08-30 15:32:25 -0700

    0.3.2

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Onyx plugin providing read and write facilities for batch processing a Datomic d
 In your project file:
 
 ```clojure
-[com.mdrogalis/onyx-datomic "0.3.1"]
+[com.mdrogalis/onyx-datomic "0.3.2"]
 ```
 
 In your peer boot-up namespace:
--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject com.mdrogalis/onyx-datomic "0.3.1"
+(defproject com.mdrogalis/onyx-datomic "0.3.2"
   :description "Onyx plugin for Datomic"
   :url "https://github.com/MichaelDrogalis/onyx-datomic"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.mdrogalis/onyx "0.3.1"]
+                 [com.mdrogalis/onyx "0.3.2"]
                  [com.datomic/datomic-free "0.9.4755"]]
   :profiles {:dev {:dependencies [[midje "1.6.2"]
                                   [org.hornetq/hornetq-core-client "2.4.0.Final"]]
