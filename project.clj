b40597ae00b4f240391647d23525ffcc958297bf
Michael Drogalis <mjd3089@rit.edu>
2015-02-22 11:00:45 -0800

    Bump version.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.mdrogalis/onyx "0.5.0"]]
+                 [com.mdrogalis/onyx "0.5.2"]]
   :profiles {:dev {:dependencies [[midje "1.6.2"]
                                   [org.hornetq/hornetq-core-client "2.4.0.Final"]
                                   [com.datomic/datomic-free "0.9.5130"]]
