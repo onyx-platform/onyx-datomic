1029d76841c312f8127c61a34fb7b8ab754698d4
Michael Drogalis <mjd3089@rit.edu>
2015-08-03 19:16:18 -0700

    0.7.0 release.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.onyxplatform/onyx-datomic "0.7.0-SNAPSHOT"
+(defproject org.onyxplatform/onyx-datomic "0.7.0"
   :description "Onyx plugin for Datomic"
   :url "https://github.com/MichaelDrogalis/onyx-datomic"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.onyxplatform/onyx "0.7.0-SNAPSHOT"]]
+                 [org.onyxplatform/onyx "0.7.0"]]
   :profiles {:dev {:dependencies [[midje "1.6.2"]
                                   [com.datomic/datomic-free "0.9.5153"]]
                    :plugins [[lein-midje "3.1.3"]]}
