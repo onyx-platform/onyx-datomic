d706cf736b897e30280b3f94e9d04150e04ce64f
OnyxBot <michael.drogalis@onyxplatform.org>
2015-08-28 16:43:54 +0000

    Update dependency org.onyxplatform/onyx to version 0.7.3-20150828_160954-g51b5cf8.
    Automatic commit by lein-traffic-control.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.7.3-20150828_152050-gd122e3e"]]
+                 [org.onyxplatform/onyx "0.7.3-20150828_160954-g51b5cf8"]]
   :profiles {:dev {:dependencies [[midje "1.7.0"]
                                   [com.datomic/datomic-free "0.9.5153"]]
                    :plugins [[lein-midje "3.1.3"]]}
