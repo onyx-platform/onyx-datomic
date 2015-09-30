84428c6b40099502e073a9224e80f532c4e0906f
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-09-30 15:17:14 +0800

    Fix release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -9,13 +9,13 @@ cd "$(dirname "$0")/.."
 
 new_version=$1
 release_branch=$2
-current_version=`lein pprint :version`
+current_version=`lein pprint :version | sed s/\"//g`
 
 # Update to release version.
 git checkout master
 lein set-version $new_version
 lein update-dependency org.onyxplatform/onyx $new_version
-sed -i '' "s/$current_version/\"$new_version\"/g" README.md
+sed -i '' 's/$current_version/"$new_version"/g' README.md
 
 git commit -am "Release version $new_version."
 git tag $new_version
@@ -24,7 +24,7 @@ git push origin master
 
 # Merge artifacts into release branch.
 git checkout $release_branch
-git merge --no-edit master -X theirs
+git merge master -X theirs
 git push origin $release_branch
 
 # Prepare next release cycle.
