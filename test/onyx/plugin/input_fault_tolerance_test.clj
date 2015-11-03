f614f3c564f4ef972bf2f952751dadabfa5598af
Michael Drogalis <mjd3089@rit.edu>
2015-11-03 07:37:43 -0800

    Sync with 0.8.0.

--- test/onyx/plugin/input_fault_tolerance_test.clj
+++ test/onyx/plugin/input_fault_tolerance_test.clj
@@ -19,8 +19,7 @@
   {:zookeeper/address "127.0.0.1:2188"
    :onyx.peer/job-scheduler :onyx.job-scheduler/greedy
    :onyx.messaging/impl :aeron
-   :onyx.messaging/peer-port-range [40200 40400]
-   :onyx.messaging/peer-ports [40199]
+   :onyx.messaging/peer-port 40200
    :onyx.messaging/bind-addr "localhost"
    :onyx.messaging/backpressure-strategy :high-restart-latency
    :onyx/id id})
--- test/onyx/plugin/output_test.clj
+++ test/onyx/plugin/output_test.clj
@@ -18,7 +18,7 @@
   {:zookeeper/address "127.0.0.1:2188"
    :onyx.peer/job-scheduler :onyx.job-scheduler/greedy
    :onyx.messaging/impl :aeron
-   :onyx.messaging/peer-port-range [40200 40400]
+   :onyx.messaging/peer-port 40200
    :onyx.messaging/bind-addr "localhost"
    :onyx.messaging/backpressure-strategy :high-restart-latency
    :onyx/id id})
