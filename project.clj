e4a49f1cf8b61ad3b43072adcaaeb74cb495c309
OnyxBot <michael.drogalis@onyxplatform.org>
2015-11-24 10:51:56 +0000

    Release version 0.8.1.0-alpha1.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-datomic "0.8.0.1"
+(defproject org.onyxplatform/onyx-datomic "0.8.1.0-alpha1"
   :description "Onyx plugin for Datomic"
   :url "https://github.com/MichaelDrogalis/onyx-datomic"
   :license {:name "Eclipse Public License"
