c7c20d92b2f3778f31ffbea4541a5f724960aff8
Gardner Vickers <gardner@vickers.me>
2016-03-14 16:43:02 -0400

    wrong key for :datomic/uri

--- test/onyx/plugin/input_log_kill_test.clj
+++ test/onyx/plugin/input_log_kill_test.clj
@@ -75,7 +75,7 @@
 (deftest ^:ci datomic-input-log-kill-test
   (let [{:keys [env-config peer-config datomic-config]}
         (read-config (clojure.java.io/resource "config.edn") {:profile :test})
-        db-uri (str (:datomic/config datomic-config) (java.util.UUID/randomUUID))
+        db-uri (str (:datomic/uri datomic-config) (java.util.UUID/randomUUID))
         job (build-job db-uri 10 1000)
         {:keys [persist]} (core-async/get-core-async-channels job)
         job-id (atom nil)
