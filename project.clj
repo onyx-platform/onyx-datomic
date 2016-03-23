89655e29113fe11aa3ef1525246270bfba295d56
OnyxBot <michael.drogalis@onyxplatform.org>
2016-03-23 18:43:24 +0000

    Prepare for next release cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-datomic "0.9.0.0"
+(defproject org.onyxplatform/onyx-datomic "0.9.0.1-SNAPSHOT"
   :description "Onyx plugin for Datomic"
   :url "https://github.com/onyx-platform/onyx-datomic"
   :license {:name "Eclipse Public License"
