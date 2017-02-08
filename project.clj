d962dce4617d4aeb3c44bf39976edc5cfa3e55bf
OnyxBot <michael.drogalis@onyxplatform.org>
2017-02-08 07:37:50 +0000

    Release version 0.10.0.0-alpha4.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-datomic "0.10.0.0-alpha1"
+(defproject org.onyxplatform/onyx-datomic "0.10.0.0-alpha4"
   :description "Onyx plugin for Datomic"
   :url "https://github.com/onyx-platform/onyx-datomic"
   :license {:name "Eclipse Public License"
@@ -13,7 +13,7 @@
                              :sign-releases false}}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.10.0-alpha1"]]
+                 [org.onyxplatform/onyx "0.10.0-alpha4"]]
   :test-selectors {:default (complement :ci)
                    :ci :ci
                    :all (constantly true)}
