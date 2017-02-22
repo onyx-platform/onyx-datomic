f84d16e22b498fc01d3c77c306c7b98919a49622
Lucas Bradstreet <lucasbradstreet@gmail.com>
2017-02-21 21:30:31 -0800

    Use new plugin interface.

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
                              :sign-releases false}}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.10.0-beta4"]]
+                 [org.onyxplatform/onyx "0.10.0-SNAPSHOT"]]
   :test-selectors {:default (complement :ci)
                    :ci :ci
                    :all (constantly true)}
